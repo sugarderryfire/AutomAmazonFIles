0f27441c7792ade2fa933dc6d09a572cf78423db
sugarderryfire <45015729+sugarderryfire@users.noreply.github.com>
2018-11-27 12:09:38 -0800

    Add files via upload

--- aws.py
+++ aws.py
@@ -7,11 +7,16 @@ import sys
 import paramiko
 import base64
 import os
+import random
+import subprocess
 
 
-hostIP="54.152.202.91"
+
+#hostIP="54.152.202.91"
 runningInstances=[]
-regionLIst=["us-east-1","us-east-2","us-west-1","us-west-2","eu-west-2","ap-southeast-2","ap-northeast-1","eu-central-1"]
+regionList=["us-east-1","us-east-2","us-west-1","us-west-2","eu-west-2","ap-southeast-2","ap-northeast-1","eu-central-1"]
+imageList=["ami-0ac019f4fcb7cb7e6","ami-0f65671a86f061fcd","ami-063aa838bd7631e0b","ami-0bbe6b35405ecebdb","ami-0b0a60c0a2bd40612","ami-07a3bd4944eb120a0","ami-07ad4b1c3af1ea214","ami-0bdf93799014acdc4"]
+chosenImage=""
 
 
 def config_instances():
@@ -23,21 +28,13 @@ def config_instances():
 
 def create_instances():
     #create instances.
-    ec2_instances = ec2.create_instances(ImageId='ami-013be31976ca2c322',MinCount=1,MaxCount=1,KeyName="ec2keyInstance4")
+    ec2_instances = ec2.create_instances(ImageId="ami-0ac019f4fcb7cb7e6",MinCount=1,MaxCount=1,KeyName="ec2keyInstance4")
     time.sleep(10)
-
-    """
-    for instance in ec2_instances:
-        while instance.state != "running":
-            print '...instance is {state}'.format(state=instance.state)
-            sys.stdout.flush()
-            time.sleep(5)
-    """
     return ec2_instances
 
 
 
-def execute_command(command):
+def execute_command(hostIP):
     key = paramiko.RSAKey.from_private_key_file('ec2keyInstance4.pem')
     client = paramiko.SSHClient()
     client.load_system_host_keys()
@@ -66,7 +63,7 @@ def execute_command(command):
         print e
 
 
-def execute_command2():
+def execute_command2(hostIP):
     key = paramiko.RSAKey.from_private_key_file('ec2keyInstance4.pem')
     client = paramiko.SSHClient()
     client.load_system_host_keys()
@@ -87,7 +84,11 @@ def execute_command2():
 
 
 
-def execute_command3():
+def execute_command3(hostIP):
+    mandatoryCommand="cat scr.sh | ssh -o StrictHostKeyChecking=no -i ec2keyInstance4.pem ubuntu@"
+    mandatoryCommand=mandatoryCommand+hostIP
+#    mandatoryCommand=mandatoryCommand+ " |"
+    #mandatoryCommand=mandatoryCommand + " && exit"
     key = paramiko.RSAKey.from_private_key_file('ec2keyInstance4.pem')
     client = paramiko.SSHClient()
     client.load_system_host_keys()
@@ -103,7 +104,9 @@ def execute_command3():
 	execute(client,'sudo apt -y install xvfb')
 #	execute(client,comm1)
         #execute(client,comm2)
-	os.system('cat scr.sh | ssh -i ec2keyInstance4.pem ubuntu@54.152.202.91')
+	#os.system(mandatoryCommand)
+	returned_output = subprocess.check_output(mandatoryCommand)	
+	print 'test1v'
 	#execute(client,'python Automain.py blockchain block.chain.technology')
         # close the client connection once the job is done
    	client.close()
@@ -145,11 +148,14 @@ def execute(client,command):
 
 
 
-def terminate_instances(instances):
+def terminate_instances():
+    print 'terminate'
     #terminate instances in ec2_instances arrן
-    for instance in ec2.instances.filter(InstanceIds=ec2_instances):
-        print instance
-        instance.terminate()
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    for instance in instances:
+	instance.terminate()
+	print instance.public_ip_address
 
 
 
@@ -166,24 +172,46 @@ def get_instances():
     return instancesList
 
 
+def get_first_instance():
+    # Boto 3
+    # Use the filter() method of the instances collection to retrieve
+    instances = ec2.instances.filter(
+        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+    for instance in instances:
+	return instance.public_ip_address
 
-def check_instances():
-    runningInstances=[]
-    reservations = ec2_connection.get_all_reservations(filters={'instance-state-name': 'running'})
-    for reservation in reservations:
-        for instance in reservation.instances:
-	    print(instance.instance_id, instance.instance_type)
-	    runningInstances.append(instance.ip_address)
-	    print instance.ip_address
+    
+
+def get_currentRegion():
+    global chosenImage
+    listLen=len(regionList)
+    chosenIndex=random.randint(0,listLen-1)
+    chosenImage=imageList[chosenIndex]
+    return regionList[chosenIndex]
 
 
+def get_random(min1,max1):
+    chosenNumber=random.randint(min1-1,max1-1)
+    return chosenNumber
 
-ec2 = boto3.resource('ec2',region_name='us-east-1')
-get_instances()
+
+ec2 = boto3.resource('ec2',region_name="us-east-1")
+
 #execute_command
 #config_instances()
-#create_instances()
-time.sleep(60)
-#execute_command()
-#execute_command2()
-#execute_command3()
+while(True):
+    create_instances()
+    time.sleep(100)
+    #get_instances()
+    hostIP=str(get_first_instance())
+    print hostIP
+    execute_command(hostIP)
+    execute_command2(hostIP)
+    execute_command3(hostIP)
+    print 'test'
+    time.sleep(400)
+    #time.sleep(get_random(360,500))
+    terminate_instances()
+    time.sleep(100)
+#sys.exit(0)
+#time.sleep(get_random(300,400))
